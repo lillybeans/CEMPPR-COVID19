cc4fe1c594d96cfdac51b823216098bac65d7ac7
Lilly Tong <lilly.tong@vice.com>
2020-04-24 14:57:04 -0400

    test

---
+++ src/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql")
+
+var mysqlConnection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "cemppr",
+  database: "COVID19PollTracker",
+  multipleStatements: true
+})
+
+mysqlConnection.connect( (error) => {
+  if (!error){
+    console.log("Connection successful")
+  } else {
+    console.log(error)
+  }
+})
+
+module.exports = mysqlConnection
---
+++ src/server.js
@@ -0,0 +1,30 @@
+//Import Express, BodyParser, Handlebars
+const express = require("express")
+const bodyParser = require("body-parser")
+const exphbs = require("express-handlebars")
+
+//Import our custom files/dependencies: MySQL, our routes
+const mysqlConnection = require("./connection")
+
+const IndexRoute = require("./routes/index.js")
+const QuestionRoutes = require("./routes/questions")
+
+var app = express()
+app.engine(".hbs", exphbs({extname: '.hbs'})) //modify handlebars extension to be ".hbs"
+app.set("view engine", ".hbs")
+
+app.use(bodyParser.json())
+app.use(express.static((__dirname + '/public'))); //static files
+
+//Define our Routes:
+app.use("/", function (req,res){
+  res.render("home")
+})
+
+app.use("/submit", function (req,res){
+  res.render("submit")
+})
+
+app.use("/questions", QuestionRoutes) //use "/questions" instead of "/routes/questions" in the browser
+
+app.listen(3000) //listen to port 3000
