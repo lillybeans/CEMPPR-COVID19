fb31879b990fd5a4548eac0f6b28162806f2c157
Lilly Tong <lilly.tong@vice.com>
2020-04-24 18:47:23 -0400

    did ss change

--- src/public/js/submit.js
+++ src/public/js/submit.js
@@ -3,19 +3,20 @@ $(function() {
     var radio = $(this);
     var dropdown = $('select[name="select_existing_survey"]')
     if (radio.val() == "yes") {
-      dropdown.show()
+      dropdown.show() //prompt user to select existing survey
     } else {
       dropdown.hide()
+      $('#did_sample_size_change').addClass("d-none") //hide all subsequent questions
     }
   });
 
   $('#select_existing_survey').change(function() {
     var dropdown = $(this);
-    if (dropdown.val() != "none") {
+    if (dropdown.val() != "none") { //if user selected an existing survey
       //ask if sample size change
-      $('#haha').removeClass("d-none")
+      $('#did_sample_size_change').removeClass("d-none")
     } else {
-      $('#haha').addClass("d-none")
+      $('#did_sample_size_change').addClass("d-none")
     }
   });
 });
