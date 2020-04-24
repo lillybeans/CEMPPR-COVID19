b7f0ad12e28498cc13e365cf4e2cd498793c0660
Lilly Tong <lilly.tong@vice.com>
2020-04-24 19:51:33 -0400

    custom css is now working

--- src/public/js/submit.js
+++ src/public/js/submit.js
@@ -14,9 +14,20 @@ $(function() {
     var dropdown = $(this);
     if (dropdown.val() != "none") { //if user selected an existing survey
       //ask if sample size change
-      $('#did_sample_size_change').removeClass("d-none")
+      $('#did_sample_size_change').removeClass("d-none") //show "Did Sample Size change"
+      $("#q2no").prop("checked", true);
     } else {
       $('#did_sample_size_change').addClass("d-none")
+    }
+  });
+
+  $('input[name="did_ss_change"]').click(function() {
+    var radio = $(this);
+    var textbox = $('#sample_size_textbox')
+    if (radio.val() == "yes") {
+      textbox.removeClass("d-none")
+    } else {
+      textbox.addClass("d-none")
     }
   });
 });
