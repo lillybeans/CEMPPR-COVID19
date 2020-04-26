0738242f26620a032d9091277c3791f4b62aa50c
Lilly Tong <lilly.tong@vice.com>
2020-04-25 20:51:26 -0400

    saving my work for now

--- src/public/js/submit.js
+++ src/public/js/submit.js
@@ -6,24 +6,27 @@ $(function() {
     var dropdown = $('select[name="select_existing_survey"]')
     if (radio.val() == "yes") {
       dropdown.fadeIn("fast") //prompt user to select existing survey
+      $('#new_survey').slideUp("fast")
     } else {
       dropdown.fadeOut("fast")
-      $('#did_sample_size_change_and_below').slideUp("fast") //hide all subsequent questions
+      $('#did_sample_size_change').slideUp("fast") //hide sample size question
+      $('#new_survey').slideDown("fast")
     }
   });
 
+// Toggling "Select Existing Survey" dropdown
   $('#select_existing_survey').change(function() {
     var dropdown = $(this);
     if (dropdown.val() != "") { //if user selected an existing survey
       //ask if sample size change
-      $('#did_sample_size_change_and_below').slideDown("fast") //show "Did Sample Size change"
-      //$("#q2no").prop("checked", true);
-      $('#sample_size_textbox').fadeOut("fast")
+      $('#did_sample_size_change').slideDown("fast") //show "Did Sample Size change"
     } else {
-      $('#did_sample_size_change_and_below').slideUp("fast")
+      $('#did_sample_size_change').slideUp("fast")
+      $('#new_survey').slideUp("fast")
     }
   });
 
+  // If answered "Did sample size change for this question"
   $('input[name="did_ss_change"]').click(function() {
     var radio = $(this);
     var textbox = $('#sample_size_textbox')
