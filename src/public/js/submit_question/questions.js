3d16960b4cf3c11d432dbe5bbc6dd2ed4d92a4b4
Lilly Tong <lilly.tong@vice.com>
2020-04-27 18:57:13 -0400

    dynamic add and remove options

--- src/public/js/submit_question/questions.js
+++ src/public/js/submit_question/questions.js
@@ -1,3 +1,19 @@
-function addQuestion(){
-  
+function addAnswer(lastAddButton){
+  var lastAnswer = $(lastAddButton).closest('.answer')
+  var lastAnswerHtml = $(lastAnswer).html()
+
+  var nextAnswerHtml = "<div class='answer'>" + lastAnswerHtml + "</div>"
+
+  //Change this one to show remove button
+  $(lastAddButton).html("<ion-icon name='remove-outline'></ion-icon>")
+  $(lastAddButton).attr('onclick',"removeAnswer(this)")
+
+  var questionForm = $(lastAnswer).parent()
+  $(questionForm).append(nextAnswerHtml)
+  console.log("next html is: "+ nextAnswerHtml)
+}
+
+function removeAnswer(removeButton){
+  var answer = $(removeButton).closest('.answer')
+  $(answer).remove()
 }
