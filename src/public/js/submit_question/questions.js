2102a06ab4328e50ecb55b6a6a59772018aec89c
Lilly Tong <lilly.tong@vice.com>
2020-04-27 21:45:05 -0400

    get ready

--- src/public/js/submit_question/questions.js
+++ src/public/js/submit_question/questions.js
@@ -1,18 +1,30 @@
-function addAnswer(lastAddButton){
-  var lastAnswer = $(lastAddButton).closest('.answer')
+$(function() {
+
+$('#questions_container').on("keydown", "input.percentage", function (e) {
+  var inputValue = $(this).val();
+  if(e.keyCode == 9) { //tab pressed
+    var lastAnswer = $(this).closest('.answer')
+    addAnswer(lastAnswer)
+  }
+});
+
+})
+
+function addAnswer(lastAnswer) {
   var lastAnswerHtml = $(lastAnswer).html()
-
   var nextAnswerHtml = "<div class='answer'>" + lastAnswerHtml + "</div>"
-
-  //Change this one to show remove button
-  $(lastAddButton).html("<ion-icon name='remove-outline'></ion-icon>")
-  $(lastAddButton).attr('onclick',"removeAnswer(this)")
 
   var questionForm = $(lastAnswer).parent()
   $(questionForm).append(nextAnswerHtml)
+
+  console.log("next html is "+nextAnswerHtml)
+
+  var removeButtonHtml = "<button type='button' tabindex=-1 class='btn btn-dark pb-1' onclick='removeAnswer(this)'>\n"+
+                      "<ion-icon name='remove-outline'></ion-icon></button>"
+  $(questionForm).children('.answer').last().find('.button_container').html(removeButtonHtml)
 }
 
-function removeAnswer(removeButton){
+function removeAnswer(removeButton) {
   var answer = $(removeButton).closest('.answer')
   $(answer).remove()
 }
