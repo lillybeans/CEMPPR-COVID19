71575a54f9be18e84d8404f1a41704df6a3760af
Lilly Tong <lilly.tong@vice.com>
2020-05-05 22:19:51 -0400

    better search

--- src/public/js/questionDetails.js
+++ src/public/js/questionDetails.js
@@ -1,26 +1,31 @@
-function searchQuestion(input){
+function searchQuestion(input) {
   var text = $(input).val()
   var searchResultsDiv = $(input).parentsUntil('.table').parent().find('.search_results').first()
   var questionsListDiv = $(input).parentsUntil('.table').parent().find('.questions_list').first()
 
-  //SEARCH
-  $.post("/database/search/questions", text)
-  .then(function(response) {
-    data = response
-    return $.get("/dynamic_views/searchQuestionResults.hbs")
-  }).then(function(src) {
-    var searchQuestionResultsHtml = Handlebars.compile(src)(data)
+  if (text == "") {
+    $(searchResultsDiv).addClass("hide")
+    $(questionsListDiv).removeClass("hide")
+  } else {
+    //SEARCH
+    $.post("/database/search/questions", text)
+      .then(function(response) {
+        data = response
+        return $.get("/dynamic_views/searchQuestionResults.hbs")
+      }).then(function(src) {
+        var searchQuestionResultsHtml = Handlebars.compile(src)(data)
 
-    $(questionsListDiv).addClass("hide")
-    $(searchResultsDiv).html(searchQuestionResultsHtml)
+        $(questionsListDiv).addClass("hide")
+        $(searchResultsDiv).html(searchQuestionResultsHtml)
 
-  }).fail( function() {
-    alert("Search Questions failed!")
-  })
+      }).fail(function() {
+        alert("Search Questions failed!")
+      })
+  }
 
 }
 
-function searchSurvey(input){
+function searchSurvey(input) {
   let text = $(input).val()
   console.log("search for: " + text)
 }
