9c2c131f93616ac9d185b735c84dd57423e65079
Lilly Tong <lilly.tong@vice.com>
2020-05-09 18:21:09 -0400

    better alignment

---
+++ src/public/js/database/delete.js
@@ -0,0 +1,21 @@
+function deleteQuestionWithId(id){
+  console.log("Deleting question with id:"+id)
+  $.post("/database/delete/question/"+id)
+  .done( function(data) {
+    location.reload()
+  })
+  .fail( function() {
+    alert("Delete survey failed!")
+  })
+}
+
+function deleteSurveyWithId(id) {
+  console.log("Deleting survey with id:"+id)
+  $.post("/database/delete/survey/"+id)
+  .done( function(data) {
+    location.reload()
+  })
+  .fail( function() {
+    alert("Delete survey failed!")
+  })
+}
---
+++ src/public/js/database/details.js
@@ -0,0 +1,7 @@
+function toggleQuestionDetails(element){
+  console.log("toggle question details!")
+}
+
+function toggleSurveyDetails(element){
+  console.log("toggle survey details!")
+}
